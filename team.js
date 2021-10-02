3a2ca3ef528d85597cff539f6ebf975e80948bf4
Sourav <chakrabortysourav801@gmail.com>
2021-10-02 17:42:25 +0530

    only player page left

---
+++ team.js
@@ -0,0 +1,27 @@
+console.log("Hello world")
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({ 
+      
+      loop:true,
+      margin:30,
+      autoplay:true,
+      autoplayTimeout:2000,
+      autoplayHoverPause:true,
+      responsive:{
+        0:{
+          items:1,
+          nav:false
+        },
+        600:{
+          items:2,
+          nav:false
+        },1000:{
+          items:3,
+          nav:false
+        }
+  
+      }
+  
+  
+    });
+  });
